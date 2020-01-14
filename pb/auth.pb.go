1c4d98ca5a633728beb260c6d3cf42148b07c904
Nick Kotenberg <happilymarrieddad@gmail.com>
2020-01-14 01:07:38 -0700

    finished

--- pb/auth.pb.go
+++ pb/auth.pb.go
@@ -139,6 +139,7 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
+
 // V1AuthClient is the client API for V1Auth service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
